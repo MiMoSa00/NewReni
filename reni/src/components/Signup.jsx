4120797b61e352415a31c6ba233ec7003ebda6cf
Oluwamomi <oluwamomiokpaise@gmail.com>
2024-10-08 18:36:32 -0700

    More

--- reni/src/components/Signup.jsx
+++ reni/src/components/Signup.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
-import '../App.css'
+import React, { useState } from 'react';
+import '../App.css';
 
 // imported images
-import Background from '../assets/bgimage.png'
-import logo from '../assets/logo.png'
-import { Link } from 'react-router-dom'
+import Background from '../assets/bgimage.png';
+import logo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash, faCheck } from '@fortawesome/free-solid-svg-icons';
 
@@ -22,15 +22,22 @@ const Signup = () => {
         setConfirmPassword(!confirmPassword);
     };
     const [checked, setChecked] = useState(false);
+
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 min-h-screen'>
-            <div className='hidden md:block'>
-                <img src={Background} className='object-cover' />
+            {/* Background image side */}
+            <div className='hidden md:block relative'>
+                <img 
+                    src={Background} 
+                    className='object-cover h-screen w-full fixed left-0 top-0 md:w-1/2'
+                    alt="Background"
+                />
             </div>
 
-            <div className='bg-customLightBlue py-2 min-h-full'>
+            {/* Form side */}
+            <div className='bg-customLightBlue py-2 min-h-full overflow-y-auto md:w-full sm:w-full  ml-auto'>
                 <div className='flex lg:mt-6 xl:mt-8 items-center justify-center gap-3'>
-                    <img src={logo} className='h-14 w-14' />
+                    <img src={logo} className='h-14 w-14' alt="Logo" />
                     <p className='text-customDeepBlue font-bold'>RENI.One</p>
                 </div>
                 <div className='mt-[20%] mx-4'>
@@ -39,28 +46,26 @@ const Signup = () => {
                     </div>
                     <div className='bg-gradient-to-b from-customDeepBlue to-white h-full rounded-md p-3'>
                         <form className='bg-white p-3 gap-6'>
-                            {/* <p className='text-lightGray'>Enter your email address, you 'll be sent an OTP to verify your email.</p> */}
                             <div className='grid grid-cols-1 md:grid-cols-2 gap-3 items-center'>
-                                <div className=''>
+                                <div>
                                     <label className='mb-2  text-lightGray'>First name</label>
-                                    <input className='w-[100%] px-3 block ring-1 ring-gray-400 focus:ring outline-none h-10 rounded-md' type='email' />
+                                    <input className='w-[100%] px-3 block ring-1 ring-gray-400 focus:ring outline-none h-10 rounded-md' type='text' />
                                 </div>
-                                <div className=''>
+                                <div>
                                     <label className='mb-2 block text-lightGray'>Last name</label>
-                                    <input className='w-full ring-1 px-3 ring-gray-400 focus:ring outline-none h-10 rounded-md' type='email' />
+                                    <input className='w-full ring-1 px-3 ring-gray-400 focus:ring outline-none h-10 rounded-md' type='text' />
                                 </div>
-
                             </div>
+
                             <div className='my-2'>
                                 <label className='text-lightGray font-normal mb-3 lg:mb-5'>Phone number</label>
                                 <input className='w-[100%] ring-1 px-3 py-2 mb-2 ring-lightGray rounded-md focus:ring outline-none lg:w-full ' type='text' placeholder='0918876568' />
-
-
                             </div>
+
                             <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
-                                <div className=''>
-                                    <label className='  text-lightGray'>Username</label>
-                                    <input className='w-[100%] px-3 block ring-1 ring-gray-400 h-10 focus:ring outline-none rounded-md' type='email' />
+                                <div>
+                                    <label className='text-lightGray'>Username</label>
+                                    <input className='w-[100%] px-3 block ring-1 ring-gray-400 h-10 focus:ring outline-none rounded-md' type='text' />
                                 </div>
                                 <div className='w-full lg:ml-0 '>
                                     <label className='block text-lightGray'>Gender</label>
@@ -71,38 +76,43 @@ const Signup = () => {
                                         <option value="Prefer Not to say">Prefer Not To Say</option>
                                     </select>
                                 </div>
-
                             </div>
-                            <div className=' lg:ml-0'>
+
+                            <div className='lg:ml-0'>
                                 <label className='text-lightGray font-normal mb-3 lg:mb-5'>Password</label>
-                                <div className=' ring-1 px-3 py-2 mb-2 ring-lightGray rounded-md focus:ring outline-none lg:w-full relative' >
-                                    <input className='border-none outline-none' type={showPassword ? 'text' : 'password'}
+                                <div className='ring-1 px-3 py-2 mb-2 ring-lightGray rounded-md focus:ring outline-none lg:w-full relative'>
+                                    <input
+                                        className='border-none outline-none w-full'
+                                        type={showPassword ? 'text' : 'password'}
                                         value={password}
                                         onChange={(e) => setPassword(e.target.value)}
-                                        placeholder='********' />
-                                    <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} onClick={togglePasswordVisibility} className='absolute right-2 top-3' />
+                                        placeholder='********'
+                                    />
+                                    <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} onClick={togglePasswordVisibility} className='absolute right-2 top-3 cursor-pointer' />
                                 </div>
                             </div>
-                            <div className=' lg:ml-0'>
+
+                            <div className='lg:ml-0'>
                                 <label className='text-lightGray font-normal mb-3 lg:mb-5'>Confirm Password</label>
-                                <div className=' ring-1 px-3 py-2 mb-2 ring-lightGray rounded-md focus:ring outline-none lg:w-full relative' >
-                                    <input className='border-none outline-none ' type={confirmPassword ? 'text' : 'password'}
+                                <div className='ring-1 px-3 py-2 mb-2 ring-lightGray rounded-md focus:ring outline-none lg:w-full relative'>
+                                    <input
+                                        className='border-none outline-none w-full'
+                                        type={confirmPassword ? 'text' : 'password'}
                                         value={confirm}
                                         onChange={(e) => setConfirm(e.target.value)}
-                                        placeholder='********' />
-                                    <FontAwesomeIcon icon={confirmPassword ? faEyeSlash : faEye} onClick={toggleConfirmPasswordVisibility} className='absolute right-2 top-3' />
+                                        placeholder='********'
+                                    />
+                                    <FontAwesomeIcon icon={confirmPassword ? faEyeSlash : faEye} onClick={toggleConfirmPasswordVisibility} className='absolute right-2 top-3 cursor-pointer' />
                                 </div>
                             </div>
+
                             <div className='flex gap-2 my-3 items-center'>
                                 <input
                                     type="checkbox"
-                                    className={`custom-checkbox h-6 w-6  cursor-pointer appearance-none bg-gray-300 flex items-center justify-center 
-                                      ${checked ? 'bg-green-500' : ''}`}
+                                    className={`custom-checkbox h-6 w-6 cursor-pointer appearance-none bg-gray-300 flex items-center justify-center ${checked ? 'bg-green-500' : ''}`}
                                     checked={checked}
                                     onChange={() => setChecked(!checked)}
                                 />
-
-                                {/* Show check icon when checked */}
                                 {checked && (
                                     <FontAwesomeIcon
                                         icon={faCheck}
@@ -110,38 +120,23 @@ const Signup = () => {
                                     />
                                 )}
                                 <div>
-                                    <p className='text-lightGray'>I agree to the  <span className='text-customDeepBlue font-medium'>Terms and Conditions,</span> as well as <span className='text-customDeepBlue font-medium'>Privacy Policy</span></p>
+                                    <p className='text-lightGray'>I agree to the <span className='text-customDeepBlue font-medium'>Terms and Conditions</span> and <span className='text-customDeepBlue font-medium'>Privacy Policy</span></p>
                                 </div>
                             </div>
-                            {/* <div>
-                                <button className='border rounded-md bg-customDeepBlue px-6 py-2 text-white'>
-                                    Start Verification
-                                </button>
-                            </div> */}
 
                             <div className=''>
                                 <Link to='/dashboard'>
-                                    <button className='border bg-customDeepBlue font-semibold rounded-md 
-                                 px-6 py-2 text-white w-full'>
-                                       Create account
+                                    <button className='border bg-customDeepBlue font-semibold rounded-md px-6 py-2 text-white w-full'>
+                                        Create account
                                     </button>
                                 </Link>
-
                             </div>
-                            {/* <div className='my-3'>
-                                <p className='text-center text-lightGray'>Already have an account?
-                                    <Link to='/log-in'>
-                                        <span className='text-customDeepBlue'>Login</span>
-                                    </Link>
-                                </p>
-                            </div> */}
                         </form>
                     </div>
                 </div>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default Signup
+export default Signup;
